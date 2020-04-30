84f7e871b3696750da0c9cfa986cad7b57e4dd60
Prateek Rohilla <prateekrohilla4.pr@gmail.com>
2020-04-30 16:02:25 +0530

    initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
+    path('customer/<str:pk>/', views.customer, name='customer'),
+
+    path('create_order/<str:pk>/', views.createOrder, name='createOrder'),
+    path('update_order/<str:pk>/', views.updateOrder, name='updateOrder'),
+    path('delete_order/<str:pk>/', views.delete, name='delete'),
+
+]
