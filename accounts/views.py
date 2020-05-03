85babf6f603597bf1427761d786317121e6e7192
exactlyprateek <prateekrohilla4.pr@gmail.com>
2020-05-04 02:10:19 +0530

    "login and logout"

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,58 @@
-from django.shortcuts import render,redirect
-from django.forms import inlineformset_factory
-# Create your views here.
+from django.shortcuts import render, redirect 
 from django.http import HttpResponse
-from .models import *
-from .forms import OrderForm
-from .filters import OrderFilter
+from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+from .models import *
+from .forms import OrderForm, CreateUserForm
+from .filters import OrderFilter
+
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('login')
+			
+
+		context = {'form':form}
+		return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST['username']
+			password = request.POST['password']
+			user = authenticate(request, username=username, password=password)
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'accounts/login.html', context)
+
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
 
 def home(request):
 	orders = Order.objects.all()
@@ -85,13 +132,3 @@ def delete(request, pk):
 	context = {'item': order}
 	return render(request, 'accounts/delete.html', context)
 
-def loginPage(request):
-	context = {}
-	return render(request, 'accounts/login.html', context)
-
-def registerPage(request):
-	form = UserCreationForm()
-	if form.is_valid():
-		form.save()
-	context = {'form': form}
-	return render(request, 'accounts/register.html', context)
