09d76ec26555263ffb083eb9973cdb5415c02a1d
Prateek Rohilla <prateekrohilla4.pr@gmail.com>
2020-06-09 22:48:26 +0530

    final commit

--- crm1/settings.py
+++ crm1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vz=a#71&)oysjg-@6)8=m8&%62rmn6e$_7v17-$a-t-$^_k7mn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
